3a6ad303850f15479557b9d3e1abb0f11502c0d7
LettuceDeWitt <lechugas77706gmail.com>
2024-11-26 23:18:53 -0600

    Modificando los estilos

--- footer.php
+++ footer.php
@@ -1,40 +1,58 @@
-<footer class="page-footer orange">
+<footer class="page-footer deep-orange darken-4">
   <div class="container">
     <div class="row">
+      <!-- Sobre el Proyecto -->
       <div class="col l6 s12">
         <h5 class="white-text">Sobre el Proyecto</h5>
-        <p class="grey-text text-lighten-4">Somos un equipo de estudiantes universitarios trabajando en este proyecto con mucho esfuerzo. Apreciamos cualquier apoyo que nos ayude a seguir desarrollándolo.</p>
+        <p class="grey-text text-lighten-4">
+          Somos un equipo de estudiantes universitarios trabajando en este proyecto con mucho esfuerzo. Apreciamos cualquier apoyo que nos ayude a seguir desarrollándolo.
+        </p>
       </div>
+
+      <!-- Secciones -->
       <div class="col l3 s12">
         <h5 class="white-text">Secciones</h5>
         <ul>
-          <li><a class="white-text" href="videojuegos.php">Videojuegos</a></li>
-          <li><a class="white-text" href="nosotros.php">Nosotros</a></li>
-          <li><a class="white-text" href="contacto.php">Contacto</a></li>
-          <li><a class="white-text" href="faq.php">Preguntas Frecuentes</a></li>
+          <li><a class="grey-text text-lighten-3 hoverable" href="videojuegos.php">Videojuegos</a></li>
+          <li><a class="grey-text text-lighten-3 hoverable" href="nosotros.php">Nosotros</a></li>
+          <li><a class="grey-text text-lighten-3 hoverable" href="contacto.php">Contacto</a></li>
+          <li><a class="grey-text text-lighten-3 hoverable" href="faq.php">Preguntas Frecuentes</a></li>
         </ul>
       </div>
+
+      <!-- Redes Sociales -->
       <div class="col l3 s12">
         <h5 class="white-text">Conecta con Nosotros</h5>
         <ul>
-          <li><a class="white-text" href="https://facebook.com/miProyecto">Facebook</a></li>
-          <li><a class="white-text" href="https://twitter.com/miProyecto">Twitter</a></li>
-          <li><a class="white-text" href="https://instagram.com/miProyecto">Instagram</a></li>
-          <li><a class="white-text" href="https://github.com/miProyecto">GitHub</a></li>
+          <li><a class="grey-text text-lighten-3 hoverable" href="https://www.facebook.com/lechugas.leonmagdaleno.1">
+            <i class="fab fa-facebook"></i> Facebook</a>
+          </li>
+          <li><a class="grey-text text-lighten-3 hoverable" href="https://x.com/2c4aaeb7234543f">
+            <i class="fab fa-twitter"></i> Twitter</a>
+          </li>
+          <li><a class="grey-text text-lighten-3 hoverable" href="https://www.instagram.com/mr_lettuce_de_witt/">
+            <i class="fab fa-instagram"></i> Instagram</a>
+          </li>
+          <li><a class="grey-text text-lighten-3 hoverable" href="https://github.com/LettuceDeWitt/Proyecto?tab=readme-ov-file#proyecto">
+            <i class="fab fa-github"></i> GitHub</a>
+          </li>
         </ul>
       </div>
     </div>
   </div>
+
+  <!-- Créditos -->
   <div class="footer-copyright">
-    <div class="container">
-      Hecho por <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+    <div class="container center-align">
+      <p class="grey-text text-lighten-3">
+        © 2024 Proyecto Universitario - Hecho con ❤️ por <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </p>
     </div>
   </div>
 </footer>
 
-<!--  Scripts-->
+<!-- Scripts -->
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
 <script src="js/materialize.js"></script>
 <script src="js/init.js"></script>
-</body>
-</html>
--- header.php
+++ header.php
@@ -12,10 +12,50 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
   <!-- Estilos de Materialize CSS -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection" />
 
   <!-- Tu estilo personalizado -->
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <style>
+    /* Personalización del header */
+    nav {
+      background: linear-gradient(45deg, #42a5f5, #1e88e5);
+    }
+
+    nav a {
+      font-weight: bold;
+      font-size: 1.1rem;
+    }
+
+    nav a:hover {
+      background-color: rgba(255, 255, 255, 0.2);
+      border-radius: 5px;
+    }
+
+    /* Estilo del logo */
+    #logo-container img {
+      border-radius: 50%;
+    }
+
+    /* Mensaje de bienvenida */
+    h2 {
+      text-align: center;
+      margin-top: 50px;
+      color: #1e88e5;
+      font-weight: 700;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+    }
+
+    /* Íconos en el menú móvil */
+    .sidenav li a {
+      display: flex;
+      align-items: center;
+    }
+
+    .sidenav li a i {
+      margin-right: 10px;
+    }
+  </style>
 </head>
 
 <body>
@@ -32,17 +72,15 @@
         <li><a href="#">Nosotros</a></li>
         <li><a href="#">Servicios</a></li>
         <li><a href="#">Contacto</a></li>
-        <!-- Enlace actualizado para redirigir a loginAdmin.php -->
         <li><a href="loginAdmin.php">Iniciar sesión</a></li>
       </ul>
 
       <!-- Menú para pantallas pequeñas (hamburguesa) -->
       <ul id="nav-mobile" class="sidenav">
-        <li><a href="#">Nosotros</a></li>
-        <li><a href="#">Servicios</a></li>
-        <li><a href="#">Contacto</a></li>
-        <!-- Enlace actualizado para redirigir a loginAdmin.php -->
-        <li><a href="loginAdmin.php">Iniciar sesión</a></li>
+        <li><a href="#"><i class="material-icons">people</i> Nosotros</a></li>
+        <li><a href="#"><i class="material-icons">build</i> Servicios</a></li>
+        <li><a href="#"><i class="material-icons">contact_mail</i> Contacto</a></li>
+        <li><a href="loginAdmin.php"><i class="material-icons">login</i> Iniciar sesión</a></li>
       </ul>
 
       <!-- Icono de menú (hamburguesa) para dispositivos móviles -->
@@ -54,11 +92,19 @@
   <div class="container">
     <!-- Bienvenida -->
     <h2>Bienvenido</h2>
+    <p class="center-align">Explora nuestro sitio para conocer más sobre nosotros y nuestros servicios.</p>
   </div>
 
   <!-- Agregar los scripts de Materialize -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script>
+    // Inicializar el sidenav
+    document.addEventListener('DOMContentLoaded', function () {
+      const elems = document.querySelectorAll('.sidenav');
+      M.Sidenav.init(elems);
+    });
+  </script>
 </body>
 
 </html>
